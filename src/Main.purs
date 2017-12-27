9c651c981b20877ae0042f90b6805c2f6ac799af
craigdallimore <decoy9697@gmail.com>
2017-12-28 10:59:46 +1300

    Wire up click and contxt

--- src/Main.purs
+++ src/Main.purs
@@ -1,9 +1,76 @@
 module Main where
 
 import Prelude
+
+import Control.Apply (lift2)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
+import DOM (DOM)
+import DOM.Event.EventTarget (addEventListener, eventListener)
+import DOM.Event.Types (Event)
+import DOM.HTML (window)
+import DOM.HTML.Event.EventTypes (click)
+import DOM.HTML.Types (htmlDocumentToParentNode)
+import DOM.HTML.Window (document)
+import DOM.Node.ParentNode (QuerySelector(QuerySelector), querySelector)
+import DOM.Node.Types (Element, ParentNode, elementToEventTarget)
+import Data.Maybe (Maybe, maybe)
+import Data.Traversable (sequence)
+import Data.Tuple (Tuple(..))
+import Graphics.Canvas (CANVAS, Context2D, closePath, fillPath, getCanvasElementById, getContext2D, lineTo, moveTo, setFillStyle)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
+-- Milestones
+-- - Onclick, draw a line from 0,0 to the cursor
+-- - While mousedown, draw line from 0,0 to the cursor, random colour rAF
+
+canvasClickListener :: forall eff
+  . Event
+ -> Eff (
+      dom     :: DOM
+    , console :: CONSOLE
+    | eff
+    ) Unit
+canvasClickListener e = log "clicked"
+
+bindCanvas
+  :: forall eff
+   . Tuple Element Context2D
+  -> Eff (
+        dom     :: DOM
+      , canvas  :: CANVAS
+      , console :: CONSOLE
+      | eff
+      ) Unit
+bindCanvas (Tuple canvasEl ctx) = do
+
+  addEventListener click (eventListener canvasClickListener) true (elementToEventTarget canvasEl)
+
+  _ <- setFillStyle "#ff0000" ctx
+
+  _ <- fillPath ctx $ do
+    _ <- moveTo ctx 10.0 10.0
+    _ <- lineTo ctx 20.0 20.0
+    _ <- lineTo ctx 10.0 20.0
+    closePath ctx
+
+  pure unit
+
+canvasSelector :: forall eff. ParentNode -> Eff (dom :: DOM | eff) (Maybe Element)
+canvasSelector = querySelector (QuerySelector "#c")
+
+main
+  :: forall eff
+   . Eff (
+      dom     :: DOM
+    , console :: CONSOLE
+    , canvas  :: CANVAS
+    | eff
+    ) Unit
 main = do
-  log "Hello sailor!"
+
+  mCanvEl  <- window >>= document >>= (canvasSelector <<< htmlDocumentToParentNode)
+
+  mEl      <- getCanvasElementById "c"
+  maybeCtx <- sequence (getContext2D <$> mEl)
+
+  maybe (log "Could not find canvas") bindCanvas (lift2 Tuple mCanvEl maybeCtx)
