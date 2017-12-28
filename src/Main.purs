56a0799011f50d9b64269690dec907988228c47e
craigdallimore <decoy9697@gmail.com>
2017-12-28 15:16:42 +1300

    Draw line from 0,0 to mouse on click

--- src/Main.purs
+++ src/Main.purs
@@ -3,12 +3,12 @@ module Main where
 import Prelude
 
 import Control.Apply (lift2)
-import Control.Monad.Except(runExcept)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Monad.Except (runExcept)
 import DOM (DOM)
 import DOM.Event.EventTarget (addEventListener, eventListener)
-import DOM.Event.MouseEvent (eventToMouseEvent, screenX, screenY)
+import DOM.Event.MouseEvent (eventToMouseEvent, pageX, pageY)
 import DOM.Event.Types (Event)
 import DOM.HTML (window)
 import DOM.HTML.Event.EventTypes (click)
@@ -16,26 +16,38 @@ import DOM.HTML.Types (htmlDocumentToParentNode)
 import DOM.HTML.Window (document)
 import DOM.Node.ParentNode (QuerySelector(QuerySelector), querySelector)
 import DOM.Node.Types (Element, ParentNode, elementToEventTarget)
+import Data.Either (either)
+import Data.Int (toNumber)
 import Data.Maybe (Maybe, maybe)
-import Data.Either (Either(Right))
 import Data.Traversable (sequence)
 import Data.Tuple (Tuple(..))
-import Graphics.Canvas (CANVAS, Context2D, closePath, fillPath, getCanvasElementById, getContext2D, lineTo, moveTo, setFillStyle)
+import Graphics.Canvas (CANVAS, Context2D, strokePath, getCanvasElementById, getContext2D, lineTo, moveTo, setFillStyle)
 
 -- Milestones
--- - Onclick, draw a line from 0,0 to the cursor
 -- - While mousedown, draw line from 0,0 to the cursor, random colour rAF
 
-canvasClickListener :: forall eff
-  . Event
+onCanvasClick :: forall eff
+  . Context2D
+ -> Event
  -> Eff (
       dom     :: DOM
     , console :: CONSOLE
+    , canvas  :: CANVAS
     | eff
     ) Unit
-canvasClickListener event = case runExcept (eventToMouseEvent event) of
-  Right me -> log $ "x: " <> show (screenX me) <> ", y: " <> show (screenY me)
-  _        -> log "Failed to use mouse event"
+onCanvasClick ctx event = either logError handleClick (runExcept (eventToMouseEvent event)) where
+  logError    _  = log "Failed to use mouse event"
+  handleClick me = do
+    log $ "x: " <> show x <> ", y: " <> show y
+    _ <- setFillStyle "#0000ff" ctx
+    _ <- strokePath ctx $ do
+      _ <- moveTo ctx 0.0 0.0
+      lineTo ctx (toNumber x) (toNumber y)
+    pure unit
+    where
+      x = pageX me
+      y = pageY me
+
 
 bindCanvas
   :: forall eff
@@ -48,17 +60,10 @@ bindCanvas
       ) Unit
 bindCanvas (Tuple canvasEl ctx) = do
 
-  addEventListener click (eventListener canvasClickListener) true (elementToEventTarget canvasEl)
+  let listener = eventListener (onCanvasClick ctx)
+      target   = elementToEventTarget canvasEl
 
-  _ <- setFillStyle "#ff0000" ctx
-
-  _ <- fillPath ctx $ do
-    _ <- moveTo ctx 10.0 10.0
-    _ <- lineTo ctx 20.0 20.0
-    _ <- lineTo ctx 10.0 20.0
-    closePath ctx
-
-  pure unit
+  addEventListener click listener true target
 
 canvasSelector :: forall eff. ParentNode -> Eff (dom :: DOM | eff) (Maybe Element)
 canvasSelector = querySelector (QuerySelector "#c")
